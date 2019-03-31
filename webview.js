ef947a4f121906cf3e7e678b9dfea35a344d128c
Diego La Manno <diegom@ecobee.com>
2019-03-31 14:24:39 -0400

    🎉 First commit

---
+++ webview.js
@@ -0,0 +1,15 @@
+module.exports = Franz => {
+  const getMessages = () => {
+    let count = 0;
+    const el = document.querySelector(
+      ".dashboard-top-panel .indicators .stats-group .cell-value"
+    );
+    if (el) {
+      count = parseInt(el.innerHTML, 10);
+    }
+
+    // set Franz badge
+    Franz.setBadge(count);
+  };
+  Franz.loop(getMessages);
+};
